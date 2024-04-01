0c6f3690f379707e72a1b0599f62715dd6fa0299
Richard Helper <me@rhelper.dev>
2024-04-01 10:16:39 -0400

    Rename orig to restore

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DOTFILES="$HOME/dotfiles"
 
-# Copy and remove original shell scripts
-ORIG_DIR="$DOTFILES/orig"
-mkdir -pv $ORIG_DIR && cp -v ~/.{profile,bashrc} $ORIG_DIR
-rm -v ~/.{profile,bashrc}
+# Copy and move original shell scripts
+RESTORE_DIR="$DOTFILES/restore"
+mkdir -pv $RESTORE_DIR \
+    && mv -v ~/.{profile,bashrc} $RESTORE_DIR
 
 # Place symbolic links to repo files
 ln -sv "$DOTFILES/.profile" ~/.profile
